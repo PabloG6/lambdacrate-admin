8af7013845ec8dc86bfc503fa93e136d6f22b75a
Pablo Grant <grantpablo4@gmail.com>
2024-11-30 21:51:36 -0500

    change environment variables from ghub to github

--- app/env.ts
+++ app/env.ts
@@ -25,9 +25,9 @@ export const env = createEnv({
     PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
     API_URL: process.env.API_URL,
-    GITHUB_URL: process.env.GHUB_URL,
-    GITHUB_CLIENT_SECRET: process.env.GHUB_CLIENT_SECRET,
-    GITHUB_CLIENT_ID: process.env.GHUB_CLIENT_ID,
+    GITHUB_URL: process.env.GITHUB_URL,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   },
   emptyStringAsUndefined: true,
 });
