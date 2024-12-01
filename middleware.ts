553d0b7174039a3bb7b8c6519eb73d4b6b00076b
Pablo Grant <grantpablo4@gmail.com>
2024-11-30 22:14:52 -0500

    remove default

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from "next/server"
 
-export default function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
     const currentUser = request.cookies.get("LAMBDACRATE_AUTH")?.value
  
     if (!currentUser && request.nextUrl.pathname.startsWith('/dashboard')) {
